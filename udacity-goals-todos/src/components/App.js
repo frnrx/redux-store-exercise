56c3a4f6ee75a84631928c5715437ce2f368962a
Gabriela Froener <gabriela.froener@gmail.com>
2019-03-24 16:25:03 -0300

    moving components

--- udacity-goals-todos/src/components/App.js
+++ udacity-goals-todos/src/components/App.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import { 
+  handleInitialData
+} from '../actions/shared'
 
+class App extends React.Component {
 
-class App extends Component {
+  componentDidMount () {
+    const {dispatch} = this.props
+
+    dispatch(handleInitialData())
+  }
+
   render() {
-    return (
-      <div >
-        Hello world
+
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>
+    }
+
+    return(
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
-    );
+    )
+
+    return (
+      <div>
+        <Todos todos={todos} store={this.props.store} />
+        <Goals goals={goals} store={this.props.store} />
+      </div>
+    )
   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
